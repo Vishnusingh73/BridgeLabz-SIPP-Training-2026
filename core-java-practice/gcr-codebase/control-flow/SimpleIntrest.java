80295768391dab4e1f61a1709f6935ce651c7d67
Vishnusingh73 <singhvishnu46760@gmail.com>
2026-06-11 12:33:35 +0530

    Add cantrol flow solutions

---
+++ core-java-practice/gcr-codebase/control-flow/SimpleIntrest.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class SimpleIntrest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter principal: ");
+        double p = sc.nextDouble();
+
+        System.out.print("Enter rate: ");
+        double r = sc.nextDouble();
+
+        System.out.print("Enter time: ");
+        double t = sc.nextDouble();
+
+        double si = (p * r * t) / 100;
+
+        System.out.println("Simple Interest = " + si);
+    }
+}
